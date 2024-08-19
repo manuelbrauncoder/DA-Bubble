b617b79e3c6aba2e10e6928c4dea38742a5e4a4a
Manuel Braun <manuel.braun.coder@gmail.com>
2024-08-19 08:15:41 +0200

    implement btn for show and hide wordspace-menu and btn for toggle channel list

--- src/app/main/workspace-menu-button/workspace-menu-button.component.ts
+++ src/app/main/workspace-menu-button/workspace-menu-button.component.ts
@@ -1,13 +1,22 @@
 import { Component, inject } from '@angular/core';
 import { UiService } from '../../services/ui.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-workspace-menu-button',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './workspace-menu-button.component.html',
   styleUrl: './workspace-menu-button.component.scss'
 })
 export class WorkspaceMenuButtonComponent {
   uiService = inject(UiService);
+
+  showOpenClose(){
+    if (this.uiService.showWorkspaceMenu) {
+      return 'schließen'
+    } else {
+      return 'öffnen'
+    }
+  }
 }
