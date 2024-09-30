c0067310f8b5c1894f0e1689c83c61ae19ea54a7
Manuel Braun <manuel.braun.coder@gmail.com>
2024-09-30 21:14:39 +0200

    away-popup: improve countdown text

--- src/app/shared/away-popup/away-popup.component.ts
+++ src/app/shared/away-popup/away-popup.component.ts
@@ -5,19 +5,31 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './away-popup.component.html',
-  styleUrl: './away-popup.component.scss'
+  styleUrl: './away-popup.component.scss',
 })
 export class AwayPopupComponent {
-  countdown = 5;
+  remainingTime: number = 5 * 60; // 5 Minuten in Sekunden
+  countdownText: string = '5 Minuten';
 
-  startCountdown(){
-    setInterval(() => {
-      this.countdown--;
-    }, 60000);
+  startCountdown() {
+    const interval = setInterval(() => {
+      this.remainingTime--;
+
+      if (this.remainingTime <= 0) {
+        clearInterval(interval);
+        this.countdownText = '< 1 Minute';
+      } else if (this.remainingTime > 120) {
+        this.countdownText = `${Math.floor(this.remainingTime / 60)} Minuten`;
+      } else if (this.remainingTime <= 120 && this.remainingTime > 60) {
+        this.countdownText = '1 Minute';
+      } else {
+        this.countdownText = '< 1 Minute';
+      }
+
+    }, 1000); // jede Sekunde updaten
   }
 
-  constructor(){
-    this.countdown = 5;
+  constructor() {
     this.startCountdown();
   }
 }
