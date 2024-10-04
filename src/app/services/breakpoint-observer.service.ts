6af9f711062033e0edb17d0b6be884924d262b65
Manuel Braun <manuel.braun.coder@gmail.com>
2024-10-04 19:46:01 +0200

    breakpoint-observer-service: clean code

--- src/app/services/breakpoint-observer.service.ts
+++ src/app/services/breakpoint-observer.service.ts
@@ -8,65 +8,71 @@ import { UiService } from './ui.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BreakpointObserverService {
-
   uiService = inject(UiService);
 
   breakpointsToObserve = [
     `(max-width: 1399px)`,
     `(max-width: 999px)`,
     `(min-width: 1400px)`,
-    Breakpoints.HandsetLandscape
+    Breakpoints.HandsetLandscape,
   ];
 
   isMobile: boolean = false;
-  isTablet : boolean = false;
+  isTablet: boolean = false;
   isHandsetLandscape: boolean = false;
 
-
-  constructor(private responsive: BreakpointObserver, private destroyRef: DestroyRef) { }
+  constructor(
+    private responsive: BreakpointObserver,
+    private destroyRef: DestroyRef
+  ) {}
 
   /**
-   * initializes the breakpoin observer
+   * initializes the breakpoint observer
    */
-  initObserver(){
-    this.responsive.observe(this.breakpointsToObserve)
-    .pipe(takeUntilDestroyed(this.destroyRef))
-    .subscribe(state => {
-      if (state.breakpoints[`(max-width: 1399px)`]) {
-        this.isTablet = true;
-        this.isMobile = false;
-        this.uiService.showChat = true;
-      }
-      if (state.breakpoints[`(max-width: 999px)`]) {
-        this.isMobile = true;
-        this.isTablet = false;
-        this.hideInMobile();
-      }
-      if (state.breakpoints[`(min-width: 1400px)`]) {
-        this.isMobile = false;
-        this.isTablet = false;
-        this.showInDesktop();
-      }
-      if (state.breakpoints[Breakpoints.HandsetLandscape]) {
-        this.isHandsetLandscape = true;
-        
-      } else {
-        this.isHandsetLandscape = false;
-        
-      }
-    })
+  initObserver() {
+    this.responsive
+      .observe(this.breakpointsToObserve)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((state) => {
+        if (state.breakpoints[`(max-width: 1399px)`]) this.handleTabletView();
+        if (state.breakpoints[`(max-width: 999px)`]) this.handleMobileView();
+        if (state.breakpoints[`(min-width: 1400px)`]) this.handleDesktopView();
+        if (state.breakpoints[Breakpoints.HandsetLandscape]) {
+          this.isHandsetLandscape = true;
+        } else {
+          this.isHandsetLandscape = false;
+        }
+      });
   }
 
-  hideInMobile(){
+  handleTabletView() {
+    this.isTablet = true;
+    this.isMobile = false;
+    this.uiService.showChat = true;
+  }
+
+  handleMobileView() {
+    this.isMobile = true;
+    this.isTablet = false;
+    this.hideInMobile();
+  }
+
+  handleDesktopView() {
+    this.isMobile = false;
+    this.isTablet = false;
+    this.showInDesktop();
+  }
+
+  hideInMobile() {
     this.uiService.showWorkspaceMenu = true;
     this.uiService.showThread = false;
     this.uiService.showChat = false;
   }
 
-  showInDesktop(){
+  showInDesktop() {
     this.uiService.showWorkspaceMenu = true;
     this.uiService.showThread = false;
     this.uiService.showChat = true;
