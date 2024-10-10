0e68b892a4aea1ce132eeb7daef66bbc5f58ff99
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2024-10-10 09:47:09 +0200

    you can send a direct message to another user by clicking on the button in the viewOtherUsersProfilePopup

--- src/app/main/chat/popup-view-other-users-profile/popup-view-other-users-profile.component.ts
+++ src/app/main/chat/popup-view-other-users-profile/popup-view-other-users-profile.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, Input, OnInit } from '@angular/core';
 import { UserService } from '../../../services/user.service';
 import { User } from '../../../models/user.class';
 import { UiService } from '../../../services/ui.service';
+import { ConversationService } from '../../../services/conversation.service';
 
 @Component({
   selector: 'app-popup-view-other-users-profile',
@@ -13,13 +14,30 @@ import { UiService } from '../../../services/ui.service';
 export class PopupViewOtherUsersProfileComponent implements OnInit {
   userService = inject(UserService);
   uiService = inject(UiService);
+  conversationService = inject(ConversationService);
   user: User | null = null;
 
+  /**
+   * Angular lifecycle hook - Called when the component is initialized.
+   * Retrieves the data of the user whose profile is to be viewed.
+   */
   ngOnInit(): void {
     this.user = this.userService.getUserData(this.userService.uidForProfile);
   }
 
+  /**
+   * Closes the popup that displays another user's profile.
+   */
   closeOtherUsersProfile() {
     this.uiService.toggleOtherUsersProfile();
+  }
+
+  /**
+   * Initiates a direct message conversation with the specified user.
+   * @param {any} user - The user object for whom the conversation will be opened.
+   */
+  directMessageToThisUser(user: any) {
+    this.conversationService.openConversation(user);
+    this.closeOtherUsersProfile();
   }
 }
