525cd4f0ff8e29b67b055bad949251eaad74b971
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2024-10-19 18:07:08 +0200

    the online status in other profiles is dynamic now

--- src/app/main/chat/popup-view-other-users-profile/popup-view-other-users-profile.component.ts
+++ src/app/main/chat/popup-view-other-users-profile/popup-view-other-users-profile.component.ts
@@ -3,11 +3,12 @@ import { UserService } from '../../../services/user.service';
 import { User } from '../../../models/user.class';
 import { UiService } from '../../../services/ui.service';
 import { ConversationService } from '../../../services/conversation.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-popup-view-other-users-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './popup-view-other-users-profile.component.html',
   styleUrl: './popup-view-other-users-profile.component.scss'
 })
@@ -20,6 +21,7 @@ export class PopupViewOtherUsersProfileComponent implements OnInit {
   /**
    * Angular lifecycle hook - Called when the component is initialized.
    * Retrieves the data of the user whose profile is to be viewed.
+   * @returns {void} No return value.
    */
   ngOnInit(): void {
     this.user = this.userService.getUserData(this.userService.uidForProfile);
@@ -33,7 +35,23 @@ export class PopupViewOtherUsersProfileComponent implements OnInit {
   }
 
   /**
+   * Checks the current status of the user and returns the corresponding CSS class.
+   * @returns {string} The CSS class that corresponds to the user's status.
+   * Possible values: 'statusOnline', 'statusAway', or 'statusOffline'.
+   */
+  checkStatus(): string {
+    if (this.user?.status === 'online') {
+      return 'statusOnline'
+    } else if (this.user?.status === 'away') {
+      return 'statusAway'
+    } else {
+      return 'statusOffline'
+    }
+  }
+
+  /**
    * Initiates a direct message conversation with the specified user.
+   * Closes the user's profile popup after the conversation is opened.
    * @param {any} user - The user object for whom the conversation will be opened.
    */
   directMessageToThisUser(user: any) {
